c98c10ea275a403a1746c17228afe7a1fb0b64de
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 20:55:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
         }
      }
      
-    programmer_hash [:dennis_ritchie][:languages] = "C","Assembly"
+    programmer_hash [:dennis_ritchie][:languages] = ["C","Assembly"]
     programmer_hash
 end
