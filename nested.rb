4a5fcceeecd4990b14ca955559b84d298997b662
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 20:43:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].to_s
+     string = programmer_hash[:dennis_ritchie][:languages].to_s
+     answer = string
 end
 
 def adding_matz
